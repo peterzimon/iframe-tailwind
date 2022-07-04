329b3371f2ab1dd195ba5ed0048378d9d70eb934
Peter Zimon <peter.zimon@gmail.com>
2022-07-04 16:45:46 +0200

    Using Tailwind in iFrames

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import CustomIFrame from './CustomIFrame';
+import Content from './Content';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const iframeStyles = {border: 'none'};
+
+    return (
+        <div>
+            <CustomIFrame style={iframeStyles} title='A custom iframe'>
+                <Content />
+            </CustomIFrame>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Content.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Content() {
+    return (
+        <div>
+            <p className='text-3xl'>Hello</p>
+        </div>
+    );
+}
+
+export default Content;
